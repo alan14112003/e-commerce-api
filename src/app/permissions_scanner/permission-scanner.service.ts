fbb5f2b14a0efccb432266ae2c2bf1b222b18318
Admodeuscz <work.doantv@gmail.com>
2025-03-17 09:24:01 +0700

    [feat]: add auto create full permission and add full permission to admin in database

--- src/app/permissions_scanner/permission-scanner.service.ts
+++ src/app/permissions_scanner/permission-scanner.service.ts
@@ -1,5 +1,9 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { DiscoveryService, Reflector } from '@nestjs/core';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Permission } from '../permissions/entities/permission.entity';
+import { In, Repository } from 'typeorm';
+import { Role } from '../roles/entities/role.entity';
 
 @Injectable()
 export class PermissionScannerService implements OnModuleInit {
@@ -8,10 +12,50 @@ export class PermissionScannerService implements OnModuleInit {
   constructor(
     private readonly discoveryService: DiscoveryService,
     private readonly reflector: Reflector,
+    @InjectRepository(Permission)
+    private readonly permissionRepository: Repository<Permission>,
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
   ) {}
 
-  onModuleInit() {
-    console.log('Permissions:', this.getAllPermissions());
+  async onModuleInit() {
+    const requiredPermissions = this.getAllPermissions();
+    console.log('Permissions:', requiredPermissions);
+
+    // Lấy tất cả các quyền hiện có trong database
+    const existingPermissions = await this.permissionRepository.find({
+      where: { code: In(requiredPermissions) },
+    });
+
+    // Lọc ra các quyền chưa tồn tại trong database
+    const existingPermissionCodes = existingPermissions.map((p) => p.code);
+    const missingPermissions = requiredPermissions.filter(
+      (code) => !existingPermissionCodes.includes(code),
+    );
+
+    const permissionInserts = missingPermissions.map((perCode) => {
+      const [name, handle] = perCode.split('.');
+      return {
+        name: `${handle} ${name}`,
+        code: perCode,
+      };
+    });
+
+    const permissionInserted =
+      await this.permissionRepository.insert(permissionInserts);
+    console.log(permissionInserted);
+
+    const roleAdmin = await this.roleRepository.findOne({
+      where: { code: 'admin' },
+      relations: ['permissions'],
+    });
+
+    const fullPermissions = await this.permissionRepository.find();
+    if (roleAdmin) {
+      roleAdmin.permissions = fullPermissions;
+
+      await this.roleRepository.save(roleAdmin);
+    }
   }
 
   getAllPermissions(): string[] {
--- src/app/permissions_scanner/permissions_scanner.module.ts
+++ src/app/permissions_scanner/permissions_scanner.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DiscoveryModule } from '@nestjs/core';
 import { PermissionScannerService } from './permission-scanner.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Permission } from '../permissions/entities/permission.entity';
+import { Role } from '../roles/entities/role.entity';
 
 @Module({
-  imports: [DiscoveryModule],
+  imports: [DiscoveryModule, TypeOrmModule.forFeature([Permission, Role])],
   providers: [PermissionScannerService],
   exports: [PermissionScannerService],
 })
